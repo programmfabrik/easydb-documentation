a2540a2976575012813f47b65799ecfda4f1b50d
Felix Niklas <mrflix@gmail.com>
2017-10-17 16:44:04 +0200

    Popover: add faux close button

--- assets/easydb.js
+++ assets/easydb.js
@@ -1,6 +1,10 @@
 $('html').on('click', '.markdown-section img', function(){
 	var popover = document.createElement('div');
 	popover.classList.add("popover");
+	// faux close button - the popover can be closed by clicking anywhere
+	var close = document.createElement('div');
+	close.innerText = "×";
+	popover.appendChild(close);
 	var image = new Image();
 	image.classList.add("popover-image");
 	image.src = this.src;
